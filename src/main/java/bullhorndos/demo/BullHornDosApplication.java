82adfbe2e9a28245cd43c00979825665bb9f79fd
Roma021 <dtmogh@gmail.com>
2019-05-31 15:31:33 -0400

    Initial commit

---
+++ src/main/java/bullhorndos/demo/BullHornDosApplication.java
@@ -0,0 +1,13 @@
+package bullhorndos.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BullHornDosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BullHornDosApplication.class, args);
+	}
+
+}
